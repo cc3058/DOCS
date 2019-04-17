892e8af48004bcf15ced5e3e22350cdf9dee046b
diazMafer <31084398+diazMafer@users.noreply.github.com>
2019-04-17 16:51:43 -0600

    Delete old project

--- Log in/proyecto/server/Accounts.js
+++
@@ -1,9 +0,0 @@
-var postSignUp=function(userId,info){
-    console.log(userId);
-    console.log(info.profile.profession);
-    RadioNodeList.addUsersToRoles(userId,['normal-user',info.profile.profession])
-}
-
-AccountsTemplates.configue({
-    postSignUpHook:postSignUp
-});
--- Log in/proyecto/server/main.js
+++
@@ -1,8 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import '../lib/collections.js';
-import '../lib/at_config.js';
-
-
-Meteor.startup(() => {
-  // code to run on server at startup
-});
