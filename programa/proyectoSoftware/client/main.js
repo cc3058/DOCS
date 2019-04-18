3386e39583b8d1158ad71337c6d8cab154dd94ed
diazMafer <31084398+diazMafer@users.noreply.github.com>
2019-04-17 21:42:01 -0600

    Delete files

--- programa/proyectoSoftware/client/main.js
+++
@@ -1,9 +0,0 @@
-import { Template } from 'meteor/templating';
-
-import './main.html';
-
-import { Accounts } from 'meteor/accounts-base';
- 
-Accounts.ui.config({
-  passwordSignupFields: 'USERNAME_ONLY',
-});
